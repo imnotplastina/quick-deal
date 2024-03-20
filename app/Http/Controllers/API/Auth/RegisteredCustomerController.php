ac0314a7379867bfb30085161805ffc98c7a5b77
bilalov <bilalov1504@gmail.com>
2024-03-21 01:11:15 +0300

    feat: default balance value

--- app/Http/Controllers/API/Auth/RegisteredCustomerController.php
+++ app/Http/Controllers/API/Auth/RegisteredCustomerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Customer\CustomerRequest;
 use App\Http\Resources\Customer\CustomerResource;
 use App\Models\Customer;
+use App\Supports\AmountValue;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,8 +18,10 @@ class RegisteredCustomerController extends Controller
         $data = $request->validated();
 
         $customer = Customer::firstOrCreate([
-            'email' => $data['email']], $data
-        );
+            'email' => $data['email']], [
+                'name' => $data['name'],
+                'balance' => new AmountValue('0')
+            ]);
 
         Auth::login($customer);
 
